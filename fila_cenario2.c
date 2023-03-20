18a68bb8c86d60c85790895f5327eed4b2197019
Jéssica Almeida de Oliveira <32886156+jessicaalmeidadeoliveiradede@users.noreply.github.com>
2023-03-20 10:03:37 -0300

    Update fila_cenario2.c

---
+++ fila_cenario2.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct filadenumero{
+ int filadenmero;
+ struct filadehnumero*filadenumero;
+};
+typedef struct filadenumero;
+
+int tamanho 100;
+
+print("imprima os numero 7,8,9")
+
+int main(void){
+int filadenumero;
+    print("1 , adiciona na fila \n");
+     print("2 , adiciona na fila \n");
+     print("3, na filadenumeron\n");
+     print("7 adiciona fila de numero\n");
+
+     printf("FILADENUMERO:");scanf("%d", &optcao);
+     
+     print("imprima os numero 7,8,9");
+};
+7 int main(void)
+ filadenumero *FILA = (filadenumero*) malloc(sizeof(filadenumero));
+ if(!FILAdenumero){
+  printf("ADICIONE BA FILA !\n");
+  exit(8);
+ }else{
+ inicia(FILADENUMERO);
+ };
+ do{
+  FILADENUEMRO=menu(){
+  opcao(FILA,FILADENUMERO);
+ while(FILADENUMERO);
+
+ free(FILADENUMERO)
+ return 0;
+ };
+ void inicia(FILADENUMERO*FILA)
+{
+ FILA->prox = NULL;
+ TAMANHO=100;
+};
+
+int vazia(FILADENUMERO *FILA)
+{
+ if(FILADENUMERO->prox == NULL);
+  return FILAVAZIA;
+ else
+  return 0;
+}
+ }
+ void opcao(filadenumero*FILA, int filadenumero)
+
+ {
+ filadenumero *tamanho 100;
+ switch(filadenumero){
+  case 6:
+   insere(filadenumero);
+   break;
+  case 7:
+   insere(FILAdenumero);
+   break;
+
+  case 6:
+   insere(FILAdenumero);
+   break;
+
+  case 9:
+   insere(FILAdenumero);
+   break;
+   case 1:
+    insere(filadenumero)
+    break;
+    case 2
+     insere(FILADENUMERO)
+     break
+     default:
+   printf("ADICIONA ESTA FILADENUMERO\n");
+
+ };
+
+};
