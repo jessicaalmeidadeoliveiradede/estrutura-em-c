c0fa351bc09cbd6a531f5ef8969521149d700635
Jéssica Almeida de Oliveira <32886156+jessicaalmeidadeoliveiradede@users.noreply.github.com>
2023-03-20 10:01:17 -0300

    Add files via upload
    trabalho de fila

---
+++ fila.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define  tamanho_da_fila 100
+struct fila{
+ int=n;
+ int=fila;
+ 
+};
+int main cria (void.fila) {
+	fila*f=(fila*) malloc(sizeof(fila));
+	fila->1;
+	fila->2;
+	fila->3;
+	fila->7;
+	fila->9;
+	printf("adiciona uma fila de n",1);
+	printf("adiciona uma fila de n",2);
+	printf("adiciona uma fila de n",3);
+	printf("adiciona uma fila de n",7);
+	printf("adiciona uma fila de n",9);
+	fila=8
+	printf("adicina fila n",8);
+
+ void imprima fila *(fila.n){
+
+ fila->7;
+ fila->9;
+ fila->8;
+
+ printf("imprima na fila n 7",7);
+ printf("imprima para mim na filas o 9",9);
+ printf("imprima na fila n 8",8);
+ };	
+   int remove (fila.n);{
+   fila*n ;
+    if(fila==null){
+		printf("fila vazia\n")
+	}
+   }
+ return 0;
+  
+  
+}33
